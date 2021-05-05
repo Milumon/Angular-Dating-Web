ca1745cb3407ab95385c172b244e3921f5418e22
Nextiva Challenge Resolver <17993682+Milumon@users.noreply.github.com>
2021-05-05 15:03:17 -0500

    full page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,38 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainNavComponent } from './common/main-nav/main-nav.component';
+import { MainHeaderComponent } from './common/main-header/main-header.component';
+import { MainFooterComponent } from './common/main-footer/main-footer.component';
+import { ServiciosComponent } from './inicio/servicios/servicios.component';
+import { EmpresaComponent } from './inicio/empresa/empresa.component';
+import { LogrosComponent } from './inicio/logros/logros.component';
+import { PartnersComponent } from './inicio/partners/partners.component';
+import { ProductosComponent } from './inicio/productos/productos.component';
+import { LocationComponent } from './inicio/location/location.component';
+import { VideoComponent } from './inicio/video/video.component';
+import { MainHeadComponent } from './common/main-head/main-head.component';
+import { InformationComponent } from './inicio/information/information.component';
+import { PersonalComponent } from './inicio/personal/personal.component';
+import { HomeComponent } from './inicio/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainNavComponent,
+    MainHeaderComponent,
+    MainFooterComponent,
+    ServiciosComponent,
+    EmpresaComponent,
+    LogrosComponent,
+    PartnersComponent,
+    ProductosComponent,
+    LocationComponent,
+    VideoComponent,
+    MainHeadComponent,
+    InformationComponent,
+    PersonalComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
