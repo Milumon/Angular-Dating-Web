3f123a45509c248ac5088c98a05bead6fb8b1b0c
Nextiva Challenge Resolver <17993682+Milumon@users.noreply.github.com>
2021-05-06 00:21:35 -0500

    cors

--- src/app/services/noticias.service.ts
+++ src/app/services/noticias.service.ts
@@ -1,16 +1,21 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';  
 
 @Injectable({
   providedIn: 'root'
 })
 export class NoticiasService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { 
+    
+  } 
 
-  noticiasSelect(){
-    const ruta = "https://www.munisantanita.gob.pe/MicroservicioNoticias/noticia";
-    return this.http.get(ruta);
+  noticiasSelect(){ 
+
+    let headers = new HttpHeaders() 
+    headers = headers.set('Access-Control-Allow-Origin', '*');  
+    const ruta = "https://mycorsproxy-milumon.herokuapp.com/https://www.munisantanita.gob.pe/MicroservicioNoticias/noticia";
+    return this.http.get(ruta, { headers });
   }
 
 }
